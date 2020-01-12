05fe7ccfdcc58e99edad1a6253606cbdfbcbe054
Vito Royeca <vito.royeca@gmail.com>
2020-01-12 00:23:16 -0500

    Added a search field on top

--- routes/db.js
+++ routes/db.js
@@ -1,22 +1,31 @@
-const Pool = require('pg').Pool;
+const Pool = require('pg').Pool
 
 const pool = new Pool({
     user: 'user',
     host: 'localhost',
     database: 'database',
-    password: null,
+    password: 'password',
     port: 5432,
 })
 
 exports.executeQuery = function(req, res, next, text, parameters)  {
     pool.query(text, parameters)
         .then(queryResults => {
-            res.status(200).json(queryResults.rows);
+            if (req.query.web == "true") {
+                res.render(req.baseUrl.substr(1),
+                    {
+                        query: req.query.query,
+                        data: queryResults.rows
+                    })
+            } else {
+                res.status(200).json(queryResults.rows)
+            }
         })
         .catch(error => {
-            console.log(error.message);
-            console.log(parameters);
-            res.statusCode = 500;
-            res.end(error.message);
-        });
+            console.log(error.message)
+            console.log(parameters)
+            res.statusCode = 500
+            res.end(error.message)
+        })
 }
+
