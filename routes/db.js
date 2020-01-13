64e10964b23edb0a94c9e20d3b06e73c464d0aaa
Vito Royeca <vito.royeca@gmail.com>
2020-01-13 00:06:58 -0500

    Updated the sorters form

--- routes/db.js
+++ routes/db.js
@@ -1,4 +1,5 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool
+const url = require("url")
 
 const pool = new Pool({
     user: 'user',
@@ -11,14 +12,28 @@ const pool = new Pool({
 exports.executeQuery = function(req, res, next, text, parameters)  {
     pool.query(text, parameters)
         .then(queryResults => {
-            if (req.query.web == "true") {
+            if (req.query.json == "true") {
+                res.status(200).json(queryResults.rows)
+            } else {
+                if (!req.query.displayAs || req.query.displayAs === "") {
+                    req.query.displayAs = "montage"
+                }
+                if (!req.query.sortedBy || req.query.sortedBy === "") {
+                    req.query.sortedBy = "name"
+                }
+                if (!req.query.orderBy || req.query.orderBy === "") {
+                    req.query.orderBy = "ascending"
+                }
+
                 res.render(req.baseUrl.substr(1),
                     {
+                        baseUrl: req.baseUrl + url.parse(req.url).pathname,
                         query: req.query.query,
+                        displayAs: req.query.displayAs,
+                        sortedBy: req.query.sortedBy,
+                        orderBy: req.query.orderBy,
                         data: queryResults.rows
                     })
-            } else {
-                res.status(200).json(queryResults.rows)
             }
         })
         .catch(error => {
--- routes/search.js
+++ routes/search.js
@@ -22,15 +22,6 @@ router.get('/', function(req, res, next) {
     } else {
         const parameters = [query]
 
-        if (!req.query.displayAs || req.query.displayAs === "") {
-            req.query.displayAs = "montage"
-        }
-        if (!req.query.sortedBy || req.query.sortedBy === "") {
-            req.query.sortedBy = "name"
-        }
-        if (!req.query.orderBy || req.query.orderBy === "") {
-            req.query.orderBy = "ascending"
-        }
         db.executeQuery(req, res, next, text, parameters)
     }
 })
