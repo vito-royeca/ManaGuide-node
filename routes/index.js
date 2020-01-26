9fe7f527eb43f089c209fe756851e36d22fc0918
Vito Royeca <vito.royeca@gmail.com>
2020-01-26 14:04:39 -0500

    Fixed link icons.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const url = require("url")
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.render("index", {isIndex: true})
+    res.render("index",  {
+        baseUrl: req.baseUrl + url.parse(req.url).pathname
+    })
 });
 
 module.exports = router
--- routes/mobile.js
+++ routes/mobile.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const url = require("url")
 const router = express.Router()
 
 router.get('/', function(req, res, next) {
-    res.render("mobile")
+    res.render("mobile", {
+        baseUrl: req.baseUrl + url.parse(req.url).pathname
+    })
 });
 
 module.exports = router
