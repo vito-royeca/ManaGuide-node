5fb629c61fe12d0788f421241fbee9a60fe6b46c
Vito Royeca <vito.royeca@gmail.com>
2021-06-20 22:51:01 -0400

    Added sql/createOrUpdateServerUpdate.sql

---
+++ sql/createOrUpdateServerUpdate.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION createOrUpdateServerUpdate(
+    boolean) RETURNS boolean AS $$
+DECLARE
+    _full_update ALIAS FOR $1;
+
+    pkey character varying;
+BEGIN
+        INSERT INTO serverupdate(
+            full_update)
+        VALUES(
+            _full_update);
+
+    RETURN _full_update;
+END;
+$$ LANGUAGE plpgsql;
