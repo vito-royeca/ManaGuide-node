38c2cbae5a5a7da799db1c6dc98f9d95cfd62145
Vito Royeca <vito.royeca@gmail.com>
2021-07-11 21:42:23 -0400

    Fixed card button actions

--- public/scripts/card.js
+++ public/scripts/card.js
@@ -18,3 +18,12 @@ function flipCard(new_id) {
     img.style.transform = "rotate(" + rotateAngle + "deg)"
     img.setAttribute("rotangle", "" + rotateAngle) // save the rotangle attribute
 }
+
+function rotateCard(new_id) {
+    const img = document.getElementById(new_id)
+    let rotateAngle = Number(img.getAttribute("rotangle")) + 90
+
+    img.style.transition = "all 0.3s ease"
+    img.style.transform = "rotate(" + rotateAngle + "deg)"
+    img.setAttribute("rotangle", "" + rotateAngle) // save the rotangle attribute
+}
