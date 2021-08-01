93c9e0e0ec2d0e0c3f1b4d0daf4cfd0538f744eb
Vito Royeca <vito.royeca@gmail.com>
2021-08-01 11:52:35 -0400

    Removed the new_id parameter in the createOrUpdateCard* functions

--- sql/createOrUpdateCardOtherLanguages.sql
+++ sql/createOrUpdateCardOtherLanguages.sql
@@ -1,59 +1,26 @@
-CREATE OR REPLACE FUNCTION createOrUpdateCardOtherLanguages(
-    character varying
-) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION createOrUpdateCardOtherLanguages() RETURNS integer AS $$
 DECLARE
-    _new_id ALIAS FOR $1;
-
     currentRow integer := 0;
     rows integer := 0;
     row RECORD;
     row2 RECORD;
 BEGIN
-    IF lower(_new_id) = 'null' THEN
-        DELETE FROM cmcard_otherlanguage;
+    DELETE FROM cmcard_otherlanguage;
 
-        SELECT count(*) INTO rows FROM cmcard c
+    SELECT count(*) INTO rows FROM cmcard c
+        LEFT JOIN cmset s ON c.cmset = s.code
+    WHERE
+        c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
+        c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part);
+
+    RAISE NOTICE 'other languages: %/%', currentRow, rows;
+    FOR row IN SELECT new_id, c.name, cmset, cmlanguage FROM cmcard c
             LEFT JOIN cmset s ON c.cmset = s.code
         WHERE
             c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
-            c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part);
-
-        RAISE NOTICE 'other languages: %/%', currentRow, rows;
-        FOR row IN SELECT new_id, c.name, cmset, cmlanguage FROM cmcard c
-                LEFT JOIN cmset s ON c.cmset = s.code
-            WHERE
-                c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
-                c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part)
-            ORDER BY s.release_date, c.name
-        LOOP
-            FOR row2 IN SELECT new_id FROM cmcard c
-                    LEFT JOIN cmset s ON c.cmset = s.code
-                WHERE
-                    s.code = row.cmset AND
-                    c.name = row.name AND
-                    cmlanguage <> row.cmlanguage
-                ORDER BY s.release_date, c.name
-            LOOP
-                INSERT INTO cmcard_otherlanguage(
-                    cmcard,
-                    cmcard_otherlanguage)
-                VALUES(
-                    row.new_id,
-                    row2.new_id);
-            END LOOP;
-
-            currentRow := currentRow + 1;
-
-            IF currentRow % 1000 = 0 THEN
-                RAISE NOTICE 'other languages: %/%', currentRow, rows;
-            END IF;
-        END LOOP;
-
-    ELSE
-        DELETE FROM cmcard_otherlanguage WHERE cmcard = _new_id;
-        SELECT new_id, name, cmset, cmlanguage INTO row  FROM cmcard c
-            WHERE new_id = _new_id;
-
+            c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part)
+        ORDER BY s.release_date, c.name
+    LOOP
         FOR row2 IN SELECT new_id FROM cmcard c
                 LEFT JOIN cmset s ON c.cmset = s.code
             WHERE
@@ -70,8 +37,12 @@ BEGIN
                 row2.new_id);
         END LOOP;
 
-        rows := 1;
-    END IF;
+        currentRow := currentRow + 1;
+
+        IF currentRow % 1000 = 0 THEN
+            RAISE NOTICE 'other languages: %/%', currentRow, rows;
+        END IF;
+    END LOOP;
 
     RETURN rows;
 END;
--- sql/createOrUpdateCardVariations.sql
+++ sql/createOrUpdateCardVariations.sql
@@ -1,60 +1,26 @@
-CREATE OR REPLACE FUNCTION createOrUpdateCardVariations(
-    character varying
-) RETURNS integer AS $$
+CREATE OR REPLACE FUNCTION createOrUpdateCardVariations() RETURNS integer AS $$
 DECLARE
-    _new_id ALIAS FOR $1;
-
     currentRow integer := 0;
     rows integer := 0;
     row RECORD;
     row2 RECORD;
 BEGIN
-    IF lower(_new_id) = 'null' THEN
-        DELETE FROM cmcard_variation;
+    DELETE FROM cmcard_variation;
 
-        SELECT count(*) INTO rows FROM cmcard c
-                LEFT JOIN cmset s ON c.cmset = s.code
-            WHERE
-                c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
-                c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part);
+    SELECT count(*) INTO rows FROM cmcard c
+            LEFT JOIN cmset s ON c.cmset = s.code
+        WHERE
+            c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
+            c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part);
 
-        RAISE NOTICE 'variations: %/%', currentRow, rows;
-        FOR row IN SELECT new_id, c.name, cmset, cmlanguage FROM cmcard c
-                LEFT JOIN cmset s ON c.cmset = s.code
-            WHERE
-                c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
-                c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part)
-            ORDER BY s.release_date, c.name
-        LOOP
-            FOR row2 IN SELECT new_id FROM cmcard c
-                    LEFT JOIN cmset s ON c.cmset = s.code
-                WHERE
-                    new_id <> row.new_id AND
-                    cmset = row.cmset AND
-                    c.name = row.name AND
-                    cmlanguage = row.cmlanguage
-                ORDER BY s.release_date, c.name
-            LOOP
-                INSERT INTO cmcard_variation(
-                    cmcard,
-                    cmcard_variation)
-                VALUES(
-                    row.new_id,
-                    row2.new_id);
-            END LOOP;
-
-            currentRow := currentRow + 1;
-
-            IF currentRow % 1000 = 0 THEN
-                RAISE NOTICE 'variations: %/%', currentRow, rows;
-            END IF;
-        END LOOP;
-
-    ELSE
-        DELETE FROM cmcard_variation WHERE cmcard = _new_id;
-        SELECT new_id, name, cmset, cmlanguage INTO row  FROM cmcard c
-            WHERE new_id = _new_id;
-
+    RAISE NOTICE 'variations: %/%', currentRow, rows;
+    FOR row IN SELECT new_id, c.name, cmset, cmlanguage FROM cmcard c
+            LEFT JOIN cmset s ON c.cmset = s.code
+        WHERE
+            c.new_id NOT IN (SELECT cmcard_face FROM cmcard_face) AND
+            c.new_id NOT IN (SELECT cmcard_part FROM cmcard_component_part)
+        ORDER BY s.release_date, c.name
+    LOOP
         FOR row2 IN SELECT new_id FROM cmcard c
                 LEFT JOIN cmset s ON c.cmset = s.code
             WHERE
@@ -72,8 +38,12 @@ BEGIN
                 row2.new_id);
         END LOOP;
 
-        rows := 1;
-    END IF;    
+        currentRow := currentRow + 1;
+
+        IF currentRow % 1000 = 0 THEN
+            RAISE NOTICE 'variations: %/%', currentRow, rows;
+        END IF;
+    END LOOP;
 
     RETURN rows;
 END;
