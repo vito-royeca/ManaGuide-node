cad23b4427d0e02a679b494f7e8fdcf330970b76
Vito Royeca <vito.royeca@gmail.com>
2024-02-25 23:39:43 -0500

    Added artists parameter to advanceSerarcgCards

--- routes/advancesearch.js
+++ routes/advancesearch.js
@@ -7,19 +7,25 @@ const e = require('express')
 
 // select by query
 router.get('/', function(req, res, next) {
-    const sql = 'SELECT * from advanceSearchCards($1,$2,$3,$4,$5,$6,$7)'
+    const sql = 'SELECT * from advanceSearchCards($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)'
     const name = req.query.name.replace(/\'/g, "''").trim()
     const colors = req.query.colors
     const rarities = req.query.rarities
     const types = req.query.types
     const keywords = req.query.keywords
+    const artists = req.query.artists
+    const pageSize = req.query.pageSize
+    const pageOffset = req.query.pageOffset
     let message = null
 
     if (name == null &&
         colors.length == 0 &&
         rarities.length == 0 &&
         types.length == 0 &&
-        keywords.length == 0) {
+        keywords.length == 0 &&
+        artists.length == 0 &&
+        pageSize == null &&
+        pageOffset == null) {
         message = "No parameters"
     }
 
@@ -32,8 +38,11 @@ router.get('/', function(req, res, next) {
             rarities,
             types,
             keywords,
+            artists,
             db.cleanSortedBy(req.query.sortedBy),
-            db.cleanOrderBy(req.query.orderBy)]
+            db.cleanOrderBy(req.query.orderBy),
+            pageSize,
+            pageOffset]
 
         db.executeQuery(req, res, next, sql, parameters, callback)
     }
